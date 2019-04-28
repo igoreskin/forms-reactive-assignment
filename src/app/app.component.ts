3ab8bdd1ca9a6556097e09c941793a75fbd81810
Igor Eskin <eskinhd@gmail.com>
2019-04-28 18:28:15 -0400

    Email custom validation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit{
     this.projectForm = new FormGroup({
       'projectData': new FormGroup({
         'projectName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email, this.invalidEmails.bind(this)])
       }),
       'projectStatus': new FormControl(null),
     })
@@ -34,6 +34,23 @@ export class AppComponent implements OnInit{
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
     if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  // A custom validator for email with a helper function:
+  // Both parts of the email, before and after the @, should consist of 
+  // at least one and at most 3 characters comprisiong only letter, 
+  // digits and/or dots(a-z,A-Z,0-9,.). For example, "kate@test.com" and
+  // ".@." are valid, but "@test.com" is not.
+  isEmailValid(str: string) {
+    const idx = str.indexOf('@');
+    return ((/^[a-zA-Z0-9.]{1,32}$/).test(str.slice(0, idx)) && (/^[a-zA-Z0-9.]{1,32}$/).test(str.slice(idx + 1)));
+  }
+
+  invalidEmails(control: FormControl): {[s: string]: boolean} {
+    if (control.value && !this.isEmailValid(control.value)) {
+      return {'emailIsInvalid': true};
     }
     return null;
   }
